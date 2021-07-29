238c0dc9070abbe8fe87bfa36c493511b31c4d1b
Mark Nrecaj <markndrecaj@gmail.com>
2021-07-29 02:48:07 +0200

    Learned single child widget

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.blue,
+        body: SafeArea(
+          child: Container(
+            width: 100,
+            height: 50,
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+            color: Colors.white,
+            margin: EdgeInsets.all(20),
+            child: Text('Hello'),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
