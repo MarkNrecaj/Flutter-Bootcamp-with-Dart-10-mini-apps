82e9a305fa43baba94d3ef0ea584a250cc034ca3
Mark Nrecaj <markndrecaj@gmail.com>
2021-08-18 01:08:59 +0200

    Finished destini challange

---
+++ destini_challenge/lib/story.dart
@@ -0,0 +1,9 @@
+class Story {
+  String storyTitle;
+  String choice1;
+  String choice2;
+  Story({this.storyTitle, this.choice1, this.choice2});
+}
+
+
+//https://hentaihaven.xxx/watch/dark-blue-hentai/episode-2/
